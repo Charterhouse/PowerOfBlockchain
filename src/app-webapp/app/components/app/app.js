ccce351d9508445c9457bf9c5b56b3634705781a
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-05-05 14:56:10 +0200

    Show our nice logo

--- src/app-webapp/app/components/app/app.js
+++ src/app-webapp/app/components/app/app.js
@@ -2,12 +2,13 @@ import React from 'react'
 import { Col } from 'react-bootstrap'
 import { HashRouter, Route } from 'react-router-dom'
 import Home from '../home'
+import './app.css'
 
 class App extends React.Component {
   render () {
     return <HashRouter>
       <Col sm={10} smOffset={1}>
-        <h1>ÐQuestions</h1>
+        <h1><span>ÐQuestions</span></h1>
         <Route path='/' exact component={Home} />
       </Col>
     </HashRouter>
