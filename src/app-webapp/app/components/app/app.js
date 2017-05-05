d68726cd76aad35564e00efd674196900a8b175b
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-05-05 12:03:48 +0200

    Add a nice message and put content in grid column

--- src/app-webapp/app/components/app/app.js
+++ src/app-webapp/app/components/app/app.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Col } from 'react-bootstrap'
 
 // Define your routing setup in this class for correct hot-module-reloading.
 
 class App extends React.Component {
   render () {
-    return <h1>ÐQuestions</h1>
+    return <div>
+      <Col sm={10} smOffset={1}>
+        <h1>ÐQuestions</h1>
+        <p>Welcome to the Ðistributed version of 500 Questions!</p>
+      </Col>
+    </div>
   }
 }
 
