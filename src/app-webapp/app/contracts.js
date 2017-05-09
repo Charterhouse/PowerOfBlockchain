f98dbcbee8a642cb1dabbd725bd2c7d68ea26e00
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-05-09 14:36:17 +0200

    MetaMask asynchronously injects web3; wait for that before rendering the React page.

--- src/app-webapp/app/contracts.js
+++ src/app-webapp/app/contracts.js
@@ -3,20 +3,55 @@ import Web3 from 'web3'
 import appContracts from 'app-contracts'
 
 let web3Provider
-if (typeof web3 !== 'undefined') {
-  web3Provider = web3.currentProvider
-} else {
-  web3Provider = new Web3.providers.HttpProvider('http://localhost:8545')
-}
-appContracts.setProvider(web3Provider)
+let ourWeb3
 
-const ourWeb3 = new Web3(web3Provider)
-appContracts.setTransactionDefaults({
-  from: ourWeb3.eth.accounts[0],
-  gas: 600000 // you probably want to tweak this for production...
-})
+function setTransactionDefaults () {
+  appContracts.setTransactionDefaults({
+    from: ourWeb3.eth.accounts[0],
+    gas: 600000 // you probably want to tweak this for production...
+  })
+}
+
+function configure () {
+  if (typeof web3 !== 'undefined') {
+    web3Provider = web3.currentProvider
+    appContracts.setProvider(web3Provider)
+
+    ourWeb3 = new Web3(web3Provider)
+    setTransactionDefaults()
+  }
+}
+
+let account0
+function startAccountDetectTimer () {
+  setInterval(function () {
+    if (ourWeb3.eth.accounts[0] !== account0) {
+      console.info('Account change detected; update transaction defaults')
+      account0 = ourWeb3.eth.accounts[0]
+      setTransactionDefaults()
+    }
+  }, 1000)
+}
+
+let retryCount = 0
+function onWeb3Available (f) {
+  configure()
+  if (typeof web3 === 'undefined' && retryCount < 10) {
+    ++retryCount
+    setTimeout(() => onWeb3Available(f), 100)
+  } else {
+    account0 = ourWeb3.eth.accounts[0]
+    startAccountDetectTimer()
+    f()
+  }
+}
+
+function getWeb3 () {
+  return ourWeb3
+}
 
 module.exports = {
-  web3: ourWeb3,
+  getWeb3: getWeb3,
+  onWeb3Available: onWeb3Available,
   ...appContracts
 }
--- src/app-webapp/app/index.js
+++ src/app-webapp/app/index.js
@@ -2,6 +2,7 @@ import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
+import { onWeb3Available } from './contracts'
 
 const rootEl = document.getElementById('root')
 const render = Component =>
@@ -12,7 +13,7 @@ const render = Component =>
     rootEl
   )
 
-render(App)
+onWeb3Available(() => render(App))
 if (module.hot) {
   module.hot.accept()
 }
