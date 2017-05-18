4f2ec22dc9e41d06837cd1b9cb8ac4d6a67e1f84
Stefan van den Oord <stefan.van.den.oord@philips.com>
2017-05-18 11:39:36 +0200

    Starting point: create new contract instead of checking deployed()

--- src/app-contracts/test/DQuestions.js
+++ src/app-contracts/test/DQuestions.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect
 const DQuestions = artifacts.require('DQuestions.sol')
 
 contract('DQuestions', function () {
-  it('is deployed', async function () {
-    expect(await DQuestions.deployed()).to.exist
+  it('can be constructed', async function () {
+    expect(await DQuestions.new()).to.exist
   })
 })
