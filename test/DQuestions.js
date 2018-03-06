54527e16364900f86d555dab5e9083e381cbca91
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-03-06 10:53:04 +0100

    Refactor: remove test that has become superfluous

--- test/DQuestions.js
+++ test/DQuestions.js
@@ -3,10 +3,6 @@ const expect = require('chai').expect
 const DQuestions = artifacts.require('DQuestions.sol')
 
 contract('DQuestions', function () {
-  it('can be constructed', async function () {
-    expect(await DQuestions.new()).to.exist()
-  })
-
   it('has no questions initially', async function () {
     const questions = await DQuestions.new()
     const numberOfQuestions = await questions.numberOfQuestions()
