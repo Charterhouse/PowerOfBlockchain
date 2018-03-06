7d61947a3899188a2b2f25b36bf84f2c06b563c4
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-03-06 10:56:58 +0100

    Increment numberOfQuestions when adding question

--- contracts/DQuestions.sol
+++ contracts/DQuestions.sol
@@ -2,4 +2,8 @@ pragma solidity ^0.4.18;
 
 contract DQuestions {
     uint public numberOfQuestions;
+
+    function add(string question) public {
+        numberOfQuestions++;
+    }
 }
--- test/DQuestions.js
+++ test/DQuestions.js
@@ -3,9 +3,25 @@ const expect = require('chai').expect
 const DQuestions = artifacts.require('DQuestions.sol')
 
 contract('DQuestions', function () {
+  let questions
+
+  beforeEach(async function () {
+    questions = await DQuestions.new()
+  })
+
   it('has no questions initially', async function () {
-    const questions = await DQuestions.new()
     const numberOfQuestions = await questions.numberOfQuestions()
     expect(numberOfQuestions.toNumber()).to.equal(0)
+  })
+
+  context('when adding a question', function () {
+    beforeEach(async function () {
+      await questions.add('Who is Satoshi Nakamoto?')
+    })
+
+    it('increases the number of questions', async function () {
+      const numberOfQuestions = await questions.numberOfQuestions()
+      expect(numberOfQuestions.toNumber()).to.equal(1)
+    })
   })
 })
