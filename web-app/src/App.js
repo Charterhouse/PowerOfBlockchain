c87defd78c5ba897e1f4bd8c8bd308fc59787e5f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2018-03-06 09:30:13 +0100

    Added heading with image to App

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,6 +4,8 @@ import { Home } from 'pages/home'
 import { Accounts } from 'pages/accounts'
 import { DApp } from 'pages/dapp'
 import { Web3 } from 'components/web3'
+import { Col } from 'react-bootstrap'
+import './App.css'
 
 const renderComponent = (Component, routeProps, web3Props) => (
   <Component {...web3Props}
@@ -20,9 +22,12 @@ export default () =>
       if (web3IsReady(web3Props)) {
         return (
           <div>
-            <Route exact path='/' component={Home} />
-            <Route path='/dapp' render={routeProps => renderComponent(DApp, routeProps, web3Props)} />
-            <Route path='/accounts' render={routeProps => renderComponent(Accounts, routeProps, web3Props)} />
+            <Col sm={10} smOffset={1}>
+              <h1><span>ÐQuestions</span></h1>
+              <Route exact path='/' component={Home} />
+              <Route path='/dapp' render={routeProps => renderComponent(DApp, routeProps, web3Props)} />
+              <Route path='/accounts' render={routeProps => renderComponent(Accounts, routeProps, web3Props)} />
+            </Col>
           </div>
         )
       } else {
