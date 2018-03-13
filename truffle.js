1a07516ef73bb5be55ca5649d26c0fac9c408e08
Mark Spanbroek <mark@spanbroek.net>
2018-03-13 13:50:07 +0100

    Add in-memory testing network

--- truffle.js
+++ truffle.js
@@ -1,9 +1,15 @@
+const ganache = require('ganache-cli')
+
 module.exports = {
   networks: {
     development: {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    testing: {
+      network_id: '*',
+      provider: ganache.provider()
     }
   },
   solc: {
