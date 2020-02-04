0dc59a6cd8a7db01ea07d58e428c1fd4efa569c9
Mark Spanbroek <mark@spanbroek.net>
2020-02-04 09:46:07 +0100

    wip

--- contracts/DQuestions.sol
+++ contracts/DQuestions.sol
@@ -1,3 +1,15 @@
 pragma solidity ^0.5.12;
 
-contract DQuestions {}
+contract DQuestions {
+    string[] questions;
+    uint256 public numberOfQuestions;
+
+    function add(string memory question) public {
+        questions.push(question);
+        numberOfQuestions++;
+    }
+
+    function getQuestion(uint256 index) public view returns (string memory) {
+        return questions[index];
+    }
+}
--- test/DQuestions.js
+++ test/DQuestions.js
@@ -3,7 +3,26 @@ const { expect } = require('chai')
 const DQuestions = artifacts.require('DQuestions.sol')
 
 contract('DQuestions', function (accounts) {
-  it('can be constructed', async function () {
-    expect(await DQuestions.new()).to.exist()
+  let questions
+
+  beforeEach(async function () {
+    questions = await DQuestions.new()
+  })
+
+  it('has no questions initially', async function () {
+    const amount = await questions.numberOfQuestions()
+    expect(amount.toNumber()).to.equal(0)
+  })
+
+  it('increments number of questions when one is added', async function () {
+    await questions.add('Who is Satoshi Nakamoto?')
+    const amount = await questions.numberOfQuestions()
+    expect(amount.toNumber()).to.equal(1)
+  })
+
+  it('can retrieve the question', async function () {
+    await questions.add('Who is Satoshi Nakamoto?')
+    const retrieved = await questions.getQuestion(0)
+    expect(retrieved).to.equal('Who is Satoshi Nakamoto?')
   })
 })
